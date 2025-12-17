c1abad7aa2e4c50b5fbcd8c42bec0bed230abdcd
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-12-17 19:04:16 +0530

    Functional Component and Class Component in React

--- src/React_3/classCounter.jsx
+++ src/React_3/classCounter.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 export default class ClassCounter extends Component {
     // 1. state declare in an object -> initialize it's value in constructor
     constructor(props) {
+        console.log("1. Constructor Called");
         // calling super class constructor
         super(props);
         this.state = {
@@ -22,8 +23,24 @@ export default class ClassCounter extends Component {
         })
     }
 
+    // 3. run once just after render
+    componentDidMount() {
+        console.log("3. Component Did Mount");
+    }
+
+    // 4. run after every re-render
+    componentDidUpdate(){
+        console.log("4. Component Did Update");
+    }
+
+    // 5. Component will Unmount
+    componentWillUnmount(){
+        console.log("5. Component Will Unmount (When component is removed then it will be called)");
+    }
+
     // 2. render method
     render() {
+        console.log("2. Render Method Called");
         return (<>
             <h1>Class Counter</h1>
             <h2>{this.state.count}</h2>
